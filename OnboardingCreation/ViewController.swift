59b4149caad825a9f3138cbaadaa08c76fec7495
Cesar Humberto Grifaldo Garcia <cesar.grifaldo@elektra.com.mx>
2023-10-17 16:12:15 -0600

    Initial Commit

---
+++ OnboardingCreation/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  OnboardingCreation
+//
+//  Created by Cesar Humberto Grifaldo Garcia on 17/10/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
